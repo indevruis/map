fce2bc88580715a67502b7b38a545a1da9e28e41
indevruis <indevruis@gmail.com>
2023-02-23 13:18:44 +0900

    [feat] profile 구현 중

---
+++ js/profile.js
@@ -0,0 +1,45 @@
+const birthday = '2002.02.04';
+const work = '학생';
+const website = 'https://github.com/indevruis';
+const about = '그 외 정보';
+
+const information = document.createElement('div');
+information.className = 'side-information';
+information.innerHTML = `
+    <ul>
+      <li>
+        <div>
+          Birthday
+          <span>${birthday}</span>
+        </div>
+      </li>
+      <li>
+        <div>
+          Work
+          <span>${work}</span>
+        </div>
+      </li>
+      <li>
+        <div>
+          Website
+          <span><a href="${website}">${website}</a></span>
+        </div>
+      </li>
+      <li>
+        <div>
+          About
+          <span>${about}</span>
+      </li>
+    </ul>
+  `;
+
+document.querySelector('.main-contents').appendChild(information);
+
+const photos = document.createElement('div');
+
+photos.className = 'main-photos';
+photos.innerHTML  = `
+  <img src="">
+  `;
+
+document.querySelector('.main-contents').appendChild(photos);
