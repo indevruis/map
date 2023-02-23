51661acfc13583284697220a010e27885edfb0a4
indevruis <indevruis@gmail.com>
2023-02-23 12:03:18 +0900

    [feat] navbar 클릭 기능 추가

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+const navBtn = document.querySelectorAll('.navBtn');
+
+navBtn.forEach((btn)=>{
+  btn.addEventListener('click', ()=>{
+    navBtn.forEach((rmbtn)=>{
+      rmbtn.classList.remove('onclick-navBtn');
+    })
+    btn.classList.add('onclick-navBtn')
+  })
+})
