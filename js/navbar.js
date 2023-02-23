c1d0b2115cd67523f82f513e39ccfe6f376092a8
indevruis <indevruis@gmail.com>
2023-02-23 20:03:49 +0900

    [feat] navbar 파일 추가

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,27 @@
+const navbar = document.createElement('div');
+navbar.className = 'navbar';
+navbar.innerHTML = `
+  <a>
+  <div class="navBtn">
+    <i class="far fa-user"></i>
+    <span>Profile</span>
+  </div>
+  </a>
+  <a>
+  <div class="navBtn">
+    <i class="fas fa-th"></i>
+    <span>Posting</span>
+  </div>
+  </a>
+  <a>
+  <div class="navBtn">
+    <i class="fas fa-map-marker-alt"></i>
+    <span>Map</span>
+  </div>
+  </a>
+  `;
 
+document.querySelector('main-navbar').appendChild(navbar);
 
 const navBtn = document.querySelectorAll('.navBtn');
 
